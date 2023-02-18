1e3196040d9c7248b20771755bb8cc0483abfd37
skinnyleg <haitamfox12@gmail.com>
2023-02-18 02:27:17 +0100

    finished

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations
+ * of single-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int	i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+			putchar(44);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
