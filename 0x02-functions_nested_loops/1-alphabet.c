ad3b92df38c3f133f531700a654dfa2f884ea368
Haitam Moubal <hmoubal@e2r3p5.1337.ma>
2023-02-21 16:54:15 +0100

    fnished

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ */
+
+void    print_alphabet(void)
+{
+    int i;
+
+    i = 97;
+    while (i < 'z')
+    {
+        _putchar(i);
+        i++;
+    }
+    _putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,6 @@
 #include <unistd.h>
 
 void    _putchar(int c);
-
+void    print_alphabet(void);
 #endif
 
