b8013a904a3e9a4c4fc87c90a209b52f44518b1d
Haitam Moubal <hmoubal@e2r3p5.1337.ma>
2023-02-21 18:17:14 +0100

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - calculate
+ * the sum of two parameters
+ *
+ * @a: number
+ * @b: number
+ * Return: the sum of two parametres
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
