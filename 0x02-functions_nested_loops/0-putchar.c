a388606623bb366fc40d7a005807092b57fb1d65
skinnyleg <haitammoubal7@gmail.com>
2023-02-21 07:30:12 -0800

    finished

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-/* more headers goes there */
+
 /**
- * main - prints _putchar with new
+ * _putchar - prints _putchar with new
  * line
+ */
+void    _putchar(void)
+{
+	write(1, "_putchar\n", 9);
+}
+
+/**
+ * main - calls the _putchar function
  *
  * Return: 0
  */
-/* betty style doc for function main goes there */
-
-void    _putchar(void)
-{
-    write(1, "_putchar\n" , 9);
-}
 
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@
 void    _putchar(void);
 
 #endif
+
