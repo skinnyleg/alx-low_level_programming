b8e2524717cae10513dc4065977aa69560d028f3
Haitam Moubal <hmoubal@e2r3p5.1337.ma>
2023-02-21 17:08:04 +0100

    test

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet
+ * 10 times
+ *
+ */
+
+int _islower(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    return (0);
+}
