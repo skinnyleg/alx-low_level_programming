9db811c249442b26c9f34f87a97e9bd44615d381
Haitam Moubal <hmoubal@e2r3p5.1337.ma>
2023-02-21 16:18:52 +0100

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /* more headers goes there */
 /**
  * main - prints _putchar with new
@@ -7,6 +8,12 @@
  * Return: 0
  */
 /* betty style doc for function main goes there */
+
+void    _putchar(void)
+{
+    write(1, "_putchar\n" , 9);
+}
+
 int main(void)
 {
     printf("_putchar\n");
