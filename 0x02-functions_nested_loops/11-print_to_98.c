75aef659404d917b93a99bd27ac03eb1e2b3bfcc
Haitam Moubal <hmoubal@e2r3p5.1337.ma>
2023-02-21 18:40:53 +0100

    test

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints
+ * all the numbers from n
+ * to 98
+ *
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	printf("%d\n", 98);
+}
