eccb02120777f344506c496ec388f28cc805d707
Haitam Moubal <hmoubal@e1r3p9.1337.ma>
2023-02-23 13:46:00 +0100

    task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from
+ * 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < '9')
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
