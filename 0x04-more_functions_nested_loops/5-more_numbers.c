9a4502a2646a2f5b1dce67669f4317bdc4507eb1
Haitam Moubal <hmoubal@e1r3p9.1337.ma>
2023-02-23 13:59:35 +0100

    finished

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times
+ * numbers from 0 to 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+	int n;
+	for (j = 0; j < 10; j++)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i >= 10)
+				n = i / 10;
+			else
+				n = i;
+			_putchar(n + 48);
+			if (i >= 10)
+				_putchar((i % 10) + 48);
+			i++;
+		}
+		_putchar('\n');		
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,5 +10,6 @@ int mul(int a, int b);
 void print_numbers(void);
 void    _putchar(int c);
 void print_most_numbers(void);
+void more_numbers(void);
 #endif /* MAIN_H */
 
