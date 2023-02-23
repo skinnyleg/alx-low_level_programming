8423b46b20db76c3db67e07fec494630b52f7efb
Haitam Moubal <hmoubal@e1r3p9.1337.ma>
2023-02-23 13:43:40 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply two numbers 
+ *
+ * @a: first number
+ * @b: second number
+ * Return: the new value of
+ * the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
