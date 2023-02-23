39eb0e89d8abab5f64f4e2a8cd1dfc29d688ad08
Haitam Moubal <hmoubal@e1r3p9.1337.ma>
2023-02-23 13:51:10 +0100

    fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from
+ * print_most_numbers - prints numbers from
  * 0 to 9 except 2 and 4
  *
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 48;
 
