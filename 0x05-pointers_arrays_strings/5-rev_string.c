9e4c584d664077df44ff026e3a213219ceaba8d9
Haitam Moubal <hmoubal@e2r9p3.1337.ma>
2023-02-27 18:59:20 +0100

    sixth task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - revers a string
+ * @s: a pointer to a squence of
+ * characters
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char c;
+
+	while (s[i] != '\0')
+		i++;
+	i--;
+	while (j < i)
+	{
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+		i--;
+		j++;	
+	}
+}
