f7de7474939246e208f82deaeaded07b291e633b
Haitam Moubal <hmoubal@e2r9p3.1337.ma>
2023-02-27 18:30:39 +0100

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of
+ * a and b
+ *
+ * @a: pointer to an int variable
+ * @b: pointer to an int variable
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
