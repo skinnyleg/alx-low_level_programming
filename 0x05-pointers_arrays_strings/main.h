0b5f72f25c76e30f97e519ae3fd8874df88822e5
Haitam Moubal <hmoubal@e2r9p3.1337.ma>
2023-02-27 18:38:06 +0100

    fifth task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@
 
 #include <stdio.h>
 #include <unistd.h>
-
+void    _putchar(int c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 #endif /* MAIN_H */
 
