e9fb45ad33f40ed0a65750ad7f8d7546054a456f
Haitam Moubal <hmoubal@e2r9p3.1337.ma>
2023-02-27 19:50:18 +0100

    bonus

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string
+ * to a integer
+ *
+ * @s: the string to convert to integer
+ * Return: the new integer
+ */
+
+int _atoi(char *s)
+{
+	int	i;
+	int	sign;
+	int	r;
+
+	i = 0;
+	sign = 1;
+	r = 0;
+	while ((s[i] >= 9 && s[i] <= 13) || s[i] == 32)
+		i++;
+	while (s[i] == 43 || s[i] == 45)
+	{
+		if (s[i] == 45)
+			sign = -sign;
+		i++;
+	}
+	while (s[i] >= 48 && s[i] <= 57)
+	{
+		r = (r * 10) + (s[i] - 48);
+		i++;
+	}
+	return (sign * r);
+}
