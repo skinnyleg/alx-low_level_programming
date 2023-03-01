4ef8e3b57428f7e389ac911a5f2ce744f479a821
Haitam Moubal <hmoubal@e2r10p1.1337.ma>
2023-03-01 22:25:40 +0100

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,10 +19,10 @@ char *_strcat(char *dest, char *src)
 		i++;
 	while (src[j] != '\0')
 	{
-		dest[i] == src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[i] == '\0';
+	dest[i] = '\0';
 	return (dest);
 }
