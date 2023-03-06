f06b858f0100f38d710e8bcd86e8b56e49511d51
Haitam Moubal <hmoubal@e2r8p15.1337.ma>
2023-03-06 17:34:35 +0100

    task 4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,26 +12,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int ret = 0;
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	while (accept[i] != '\0')
+	while (s[i] != '\0')
 	{
 		j = 0;
-		while (s[j] != '\0')
+		while (accept[j] != '\0')
 		{
-			if (s[j] == accept[i])
-			{
-				if (j > ret)
-				{
-					ret = j + 1;
-					break;
-				}
-			}
+			if (s[i] == accept[j])
+				break;
 			j++;
 		}
+		if (accept[j] == '\0')
+			break;
 		i++;
 	}
-	return (ret);
+	return (i);
 }
