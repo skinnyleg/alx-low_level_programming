5a9d42d7a682806dd2574720a11bcb395cf24099
Haitam Moubal <hmoubal@e2r8p15.1337.ma>
2023-03-06 18:03:12 +0100

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ *
+ * @a: a two dismensional array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+	
+	while (a[i] != NULL)
+	{
+		j = 0;
+		while (a[i][j] != '\0')
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
