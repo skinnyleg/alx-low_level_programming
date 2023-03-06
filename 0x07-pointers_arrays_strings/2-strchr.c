d97898d9f2d01d6ed6415eed7d08ce6b82ed18ab
Haitam Moubal <hmoubal@e2r8p15.1337.ma>
2023-03-06 17:06:23 +0100

    task 3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for a character
+ * in a string
+ *
+ * @s: the string to search in
+ * @c: the character to look for
+ * Return: from the first occurence
+ * of the character to the end
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (NULL);
+}
