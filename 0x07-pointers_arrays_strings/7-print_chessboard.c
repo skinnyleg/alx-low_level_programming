e8135471aaf069976fc284e21ed3a728b5e56b38
Haitam Moubal <hmoubal@e2r8p15.1337.ma>
2023-03-06 18:05:29 +0100

    fixed

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,10 +11,10 @@ void print_chessboard(char (*a)[8])
 	int i = 0;
 	int j = 0;
 
-	while (a[i] != NULL)
+	while (i < 8)
 	{
 		j = 0;
-		while (a[i][j] != '\0')
+		while (j < 8)
 		{
 			_putchar(a[i][j]);
 			j++;
