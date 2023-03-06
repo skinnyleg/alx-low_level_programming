5d8e52590126190fe89993f42cc0ff79295f9a23
Haitam Moubal <hmoubal@e2r8p15.1337.ma>
2023-03-06 16:57:30 +0100

    task 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - fills the n bytes
+ * of the s string with the value
+ * of b 
+ *
+ * @s: a string
+ * @b: the value to fill the string with
+ * @n: how many bytes to fill
+ * Return: the new string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
