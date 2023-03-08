b1867bd12c267b9542d78a8add818587938eeeb4
skinnyleg <haitamfox12@gmail.com>
2023-03-08 17:45:08 +0100

    task 5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate the power
+ *
+ * @x: the base
+ * @y: the power
+ * Return: x with the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int i = x;
+
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (1);
+	i *= _pow_recursion(x, y - 1);
+	return (i);
+}
+
