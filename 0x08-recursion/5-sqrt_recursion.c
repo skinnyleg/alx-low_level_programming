93a23a45048d0b6f9bc6b080c0890c06cc341f67
skinnyleg <haitamfox12@gmail.com>
2023-03-08 18:08:19 +0100

    task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculate the
+ * square root of n
+ *
+ * @n: a number
+ * Return: square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+ 
+    int i = 1, result = 1;
+
+    while (result <= n) {
+        i++;
+        result = i * i;
+    }
+	i--;
+	if ((i * i) != n)
+		return (-1);
+    return i;
+}
+
