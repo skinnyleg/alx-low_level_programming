15e2b35e98f590a642d7f45f76bad6ad51e078a3
skinnyleg <haitamfox12@gmail.com>
2023-03-08 17:14:23 +0100

    task 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  prints a string
+ *
+ * @s: a string
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion(&s[i + 1]);
+	}
+	if (s[i] == '\0')
+		_putchar('\n');
+}
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+void    _putchar(int c);
+#endif /* MAIN_H */
+
