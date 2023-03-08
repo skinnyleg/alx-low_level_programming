e87d3bc2e9fd41faec19b2afd94041167b16bbcd
skinnyleg <haitamfox12@gmail.com>
2023-03-08 18:38:45 +0100

    task  7

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * is_prime - checks
+ * if n is prime or not
+ *
+ * @n: a number
+ * @i: a number
+ * Return: 0 if not prime
+ * or 1 is number is prime
+ */
+int is_prime(int n, int i)
+{
+	if (n % i != 0 && i < n)
+		return (is_prime(n, i + 1));
+	if (n % i == 0)
+		return (0);
+	return (1);
+}
+/**
+ * is_prime_number - checks
+ * if n is prime or not
+ *
+ * @n: a number
+ * Return: square root of n
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime(n, 2));
+}
+
