4b8261d46b3c8edec471eb24013d9afa2d1ddc65
Haitam Moubal <hmoubal@e2r9p2.1337.ma>
2023-03-10 17:49:03 +0100

    fixed betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,13 @@
 /**
  * main - prints program name
  *
+ * @argc: number of arguments
+ * @argv: two dimensional array
  * Return: 0
  */
 
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[argc - 1]);
-	return (0);	
+	return (0);
 }
